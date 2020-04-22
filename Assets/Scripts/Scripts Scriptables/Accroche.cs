a3aaab553e41099c33312f8a2bb5cb3f6a767458
Timothe <t.lafranche@rubika-edu.com>
2020-04-22 22:46:33 +0200

    Accroche réparée

--- Assets/Scripts/Scripts Scriptables/Accroche.cs
+++ Assets/Scripts/Scripts Scriptables/Accroche.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "Accroche", menuName = "Create Accroche")]
-public class Accroche : MonoBehaviour
+public class Accroche : ScriptableObject
 {
     public string texte;
 }
