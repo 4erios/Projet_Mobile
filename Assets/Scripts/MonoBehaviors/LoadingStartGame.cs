3f38e8378b3349653b77dbaea961d2a72a782250
Timothe <t.lafranche@rubika-edu.com>
2020-05-14 15:43:40 +0200

    Bug Fix

---
+++ Assets/Scripts/MonoBehaviors/LoadingStartGame.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadingStartGame : MonoBehaviour
+{
+    private void Start()
+    {
+        SaveLoadSystem.ResetGameSate();
+        LoadingScreen.HideLoadScreen();
+    }
+}
